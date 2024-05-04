22f5e51dc64df6cac9a1adea87e7e30c61e5cd10
dunglv <dunglv202@gmail.com>
2024-05-04 09:50:37 +0700

    more term for saving

--- src/main/java/vn/edu/ptit/sqa/constant/Term.java
+++ src/main/java/vn/edu/ptit/sqa/constant/Term.java
@@ -7,18 +7,22 @@ import java.util.Arrays;
 
 @Getter
 public enum Term {
+    ON_DEMAND(null),
     ONE_MONTH(1),
     TWO_MONTHS(2),
     THREE_MONTHS(3),
+    FIVE_MONTHS(5),
     SIX_MONTHS(6),
     TWELVE_MONTHS(12),
+    THIRTEEN_MONTHS(13),
+    FIFTEEN_MONTHS(15),
     EIGHT_TEEN_MONTHS(18),
     TWENTY_FOUR_MONTHS(24),
     THIRTY_SIX_MONTHS(36);
 
-    private final int totalMonth;
+    private final Integer totalMonth;
 
-    Term(int totalMonth) {
+    Term(Integer totalMonth) {
         this.totalMonth = totalMonth;
     }
 }
